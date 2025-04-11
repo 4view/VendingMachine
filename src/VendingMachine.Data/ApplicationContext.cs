beb3f957e2614405d6e7ba0e5e9beb329448cd18
4View <akochergin.2002@gmail.com>
2025-04-11 15:00:44 +0500

    feat: Start configure entities

--- src/VendingMachine.Data/ApplicationContext.cs
+++ src/VendingMachine.Data/ApplicationContext.cs
@@ -1,4 +1,13 @@
+namespace VendingMachine.Data;
+
 public class ApplicationContext : DbContext
 {
-    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+                Assembly.GetExecutingAssembly()
+        );
+    }
 }
---
+++ src/VendingMachine.Data/Configurations/BrandConfiguration.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class BrandConfiguration : IEntityTypeConfiguration<BrandEntity>
+{
+    public void Configure(EntityTypeBuilder<BrandEntity> builder)
+    {
+        builder.HasKey(p => p.Id);
+        builder.Property(p => p.Name).IsRequired().HasMaxLength(50);
+    }
+}
