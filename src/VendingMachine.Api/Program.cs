adc354d1e67dcd8fb608e71cdde49a43b7909be0
4View <akochergin.2002@gmail.com>
2025-04-14 17:27:39 +0500

    feat: Add View folder, controller and ef core design package

--- src/VendingMachine.Api/Program.cs
+++ src/VendingMachine.Api/Program.cs
@@ -5,6 +5,13 @@ var builder = WebApplication.CreateBuilder(args);
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationContext>(option => option.UseNpgsql(connection));
 
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
