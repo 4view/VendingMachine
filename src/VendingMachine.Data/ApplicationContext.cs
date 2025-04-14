904911e54caff74dbb0d7b61b738ebf3623ec537
4View <akochergin.2002@gmail.com>
2025-04-14 17:28:53 +0500

    feat: Add constructor

--- src/VendingMachine.Data/ApplicationContext.cs
+++ src/VendingMachine.Data/ApplicationContext.cs
@@ -2,6 +2,8 @@ namespace VendingMachine.Data;
 
 public class ApplicationContext : DbContext
 {
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) :
+        base (options) { }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
