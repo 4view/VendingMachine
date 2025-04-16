ddc11ef0c7d00e026294aed6040b9ffa226b1fb9
4View <akochergin.2002@gmail.com>
2025-04-16 17:34:49 +0500

    feat: Add controller for startup page

--- src/VendingMachine.Api/Controller/HomeController.cs
+++ src/VendingMachine.Api/Controller/HomeController.cs
@@ -1,7 +1,36 @@
+using VendingMachine.Core.Entities;
+using VendingMachine.Data;
+
 public class HomeController : Controller
 {
+    private readonly ApplicationContext _context;
+    public HomeController(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
     public IActionResult Index()
     {
+        var productsFromDb = _context.Set<ProductEntity>().ToList();
+        List<Product> productList = new List<Product>();
+
+        if(productsFromDb != null)
+        {
+            foreach (var product in productsFromDb)
+            {
+                var productModel = new Product()
+                {
+                    Name = product.Name,
+                    Price = product.Price,
+                    Image = product.Image,
+                    Quantity = product.Quantity
+                };
+                productList.Add(productModel);
+            }
+            return View(productList);
+        }
+
         return View();
     }
 }
