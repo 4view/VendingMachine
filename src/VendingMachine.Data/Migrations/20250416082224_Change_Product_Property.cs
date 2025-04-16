49532f5b5ead6eb26fd09cd62164a7407fd3895e
4View <akochergin.2002@gmail.com>
2025-04-16 17:32:43 +0500

    feat: Update migrations

---
+++ src/VendingMachine.Data/Migrations/20250416082224_Change_Product_Property.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VendingMachine.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_Product_Property : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
