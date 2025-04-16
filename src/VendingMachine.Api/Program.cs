ace5e6b9c669edb79aee0f343850d12fe6147e0f
4View <akochergin.2002@gmail.com>
2025-04-16 17:36:30 +0500

    feat: Add static file method

--- src/VendingMachine.Api/Program.cs
+++ src/VendingMachine.Api/Program.cs
@@ -9,6 +9,8 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}"
